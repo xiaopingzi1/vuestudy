2a8e3bce7afda0ddcef6a975497d27775c69c72c
fzp123 <15724701412@163.com>
2019-12-12 09:30:03 +0800

    文件

---
+++ src/views/pdf/fzp1206.vue
@@ -0,0 +1,115 @@
+<template>
+  <el-form ref="form" :model="form" label-width="80px">
+    <el-form-item label="活动名称">
+      <el-input v-model="form.name"></el-input>
+    </el-form-item>
+    <el-form-item label="活动区域">
+      <el-select v-model="form.region" placeholder="请选择活动区域">
+        <el-option label="区域一" value="shanghai"></el-option>
+        <el-option label="区域二" value="beijing"></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="活动时间">
+      <el-col :span="11">
+        <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+      </el-col>
+      <el-col class="line" :span="2">-</el-col>
+      <el-col :span="11">
+        <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+      </el-col>
+    </el-form-item>
+    <el-form-item label="即时配送">
+      <el-switch v-model="form.delivery"></el-switch>
+    </el-form-item>
+    <el-form-item label="活动性质">
+      <el-checkbox-group v-model="form.type">
+        <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+        <el-checkbox label="地推活动" name="type"></el-checkbox>
+        <el-checkbox label="线下主题活动" name="type"></el-checkbox>
+        <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+      </el-checkbox-group>
+    </el-form-item>
+    <el-form-item label="特殊资源">
+      <el-radio-group v-model="form.resource">
+        <el-radio label="线上品牌商赞助"></el-radio>
+        <el-radio label="线下场地免费"></el-radio>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="活动形式">
+      <el-input type="textarea" v-model="form.desc"></el-input>
+    </el-form-item>
+
+    <el-upload
+      class="upload-demo"
+      action="https://jsonplaceholder.typicode.com/posts/"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :before-remove="beforeRemove"
+      multiple
+      :limit="3"
+      :on-exceed="handleExceed"
+      :file-list="fileList"
+      v-model="form.upload"
+    >
+    <el-button size="small" type="primary">点击上传</el-button>
+    </el-upload>
+
+   <el-form-item>
+      <el-button type="primary" @click="onSubmit">立即创建</el-button>
+      <el-button>取消</el-button>
+    </el-form-item>
+
+  </el-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+        upload:''
+      }
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+      console.log(this.form)
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleExceed(files, fileList) {
+      this.$message.warning(
+        `当前限制选择 3 个文件，本次选择了 ${
+          files.length
+        } 个文件，共选择了 ${files.length + fileList.length} 个文件`
+      );
+    },
+    beforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`);
+    }
+  }
+};
+</script>
+<style scoped>
+.upload-demo{width: 50vw;margin-bottom: 100px;}
+</style>
---
+++ src/views/table/fzp1204.vue
@@ -0,0 +1,254 @@
+<template>
+  <el-table :data="tableData" style="width: 100%" row-class-name="add" @expand-change="zydescription" border >
+    <el-table-column type="expand" label="序号"  width="120">
+      <template slot-scope="props">
+        <el-table border style="width: 100%" :data="props.row.test">
+          <el-table-column label="序号" width="80" type="index"></el-table-column>
+          <el-table-column prop="name" label="商品名称" width="180"></el-table-column>
+          <el-table-column prop="shop" label="所属店铺" width="180"></el-table-column>
+          <el-table-column prop="id" label="商品 ID"></el-table-column>
+          <el-table-column prop="complate" label="是否完成">
+            <template slot-scope="props">
+              <span v-if="props.row.complate">
+                <span style="color:#00B46D" @click="a">已完成</span>
+              </span>
+              <span v-else>
+                <span style="color:#D75C89">未完成</span>
+              </span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="category" label="商品分类"></el-table-column>
+          <el-table-column label="选择日期" prop="date">
+            <template slot-scope="props">
+              <span v-if="props.row.complate">
+               <el-date-picker
+                  v-model="props.row.date"
+                  type="date"
+                  placeholder="选择日期"
+                >
+                </el-date-picker>
+              </span>
+              <span v-else>
+                <el-date-picker
+                  v-model="props.row.date"
+                  type="date"
+                  placeholder="选择日期a "
+                >
+                </el-date-picker>
+              </span>
+            </template>
+          </el-table-column>
+        </el-table>
+      </template>
+    </el-table-column>
+    <!-- <el-table-column label="序号" prop="id" type="index" width="120">
+    </el-table-column> -->
+    <el-table-column label="商品 ID" prop="id"></el-table-column>
+    <el-table-column label="商品名称" prop="name"></el-table-column>
+    <el-table-column label="描述" prop="desc"></el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          id: "12987122",
+          name: "好滋好味鸡蛋仔1",
+          category: "江浙小吃、小吃零食",
+          desc: "荷兰优质淡奶，奶香浓而不腻",
+          address: "上海市普陀区真北路",
+          shop: "王小虎夫妻店",
+          shopId: "10333",
+          date: "2019-12-12",
+          test: [
+            {
+              id: "12987122",
+              name: "好滋好味鸡蛋仔1",
+              category: "江浙小吃、小吃零食",
+              desc: "荷兰优质淡奶，奶香浓而不腻",
+              address: "上海市普陀区真北路",
+              shop: "王小虎夫妻店",
+              shopId: "10333",
+              date: "2019-12-12",
+              complate: false
+            }
+          ]
+        },
+        {
+          id: "12987123",
+          name: "好滋好味鸡蛋仔2",
+          category: "江浙小吃、小吃零食",
+          desc: "荷兰优质淡奶，奶香浓而不腻",
+          address: "上海市普陀区真北路",
+          shop: "王小虎夫妻店",
+          shopId: "10333",
+          date: "2019-12-13",
+          test: [
+            {
+              id: "12987122",
+              name: "好滋好味鸡蛋仔1",
+              category: "江浙小吃、小吃零食",
+              desc: "荷兰优质淡奶，奶香浓而不腻",
+              address: "上海市普陀区真北路",
+              shop: "王小虎夫妻店",
+              shopId: "10333",
+              date: "2019-12-12",
+              complate: true
+            }
+          ]
+        },
+        {
+          id: "12987125",
+          name: "好滋好味鸡蛋仔3",
+          category: "江浙小吃、小吃零食",
+          desc: "荷兰优质淡奶，奶香浓而不腻",
+          address: "上海市普陀区真北路",
+          shop: "王小虎夫妻店",
+          shopId: "10333",
+          date: "2019-12-12",
+          test: [
+            {
+              id: "12987122",
+              name: "好滋好味鸡蛋仔1",
+              category: "江浙小吃、小吃零食",
+              desc: "荷兰优质淡奶，奶香浓而不腻",
+              address: "上海市普陀区真北路",
+              shop: "王小虎夫妻店",
+              shopId: "10333",
+              date: "2019-12-12",
+              complate: true
+            }
+          ]
+        },
+        {
+          id: "12987126",
+          name: "好滋好味鸡蛋仔4",
+          category: "江浙小吃、小吃零食",
+          desc: "荷兰优质淡奶，奶香浓而不腻",
+          address: "上海市普陀区真北路",
+          shop: "王小虎夫妻店",
+          shopId: "10333",
+          date: "2019-12-12",
+          test: [
+            {
+              id: "12987122",
+              name: "好滋好味鸡蛋仔1",
+              category: "江浙小吃、小吃零食",
+              desc: "荷兰优质淡奶，奶香浓而不腻",
+              address: "上海市普陀区真北路",
+              shop: "王小虎夫妻店",
+              shopId: "10333",
+              date: "2019-12-12",
+              complate: true
+            }
+          ]
+        }
+      ],
+      imgType: false,
+      
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {
+    // this.setPorety();
+  },
+  created() {
+    this.setPorety();
+  },
+  methods: {
+    zydescription(row, expandedRows) {
+      // 根据点击行id判断所在索引
+      var list = this.tableData;
+      for (var i = 0; i < list.length;i++) {
+        if (list[i].id == row.id) {
+          var imgType = row.imgType;
+          this.$set(this.tableData[i], "imgType", !imgType);
+          var imgs = document.querySelectorAll(".add .el-table__expand-icon");
+            if (this.tableData[i].imgType) {
+              imgs[i].style.backgroundImage =
+                "url(https://zux2.oss-cn-beijing.aliyuncs.com/2019/12/04/fe099a9da5b043a5851dd3a2430c441c.png)";
+            } else {
+              imgs[i].style.backgroundImage =
+                "url(https://zux2.oss-cn-beijing.aliyuncs.com/2019/12/04/facb2ad1697c429a9382eb2f0aa1c0ed.png)";
+            }
+        }
+      }
+      
+    },
+    setPorety() {
+      var data = this.tableData;
+      for (var i = 0; i < data.length; i++) {
+        this.$set(data[i], "imgType", false);
+      }
+      // 图标后添加索引
+      this.$nextTick( ()=> {
+        var imgs = document.querySelectorAll(".el-table__body-wrapper .el-table__row .el-table__expand-column");
+        console.log(imgs)
+        for (let i = 0;i < imgs.length;i++) {
+          var div = document.createElement('div');
+          div.className = 'newIndex'
+          imgs[i].appendChild(div);
+          var cout = document.createTextNode(i+1)
+          div.appendChild(cout)
+        }
+      })
+
+    },
+  }
+};
+</script>
+
+<style>
+.newIndex{display:inline-block;width:32px;height:32px;line-height:32px;text-align:center;float:right;margin-right:20px;}
+.demo-table-expand {
+  font-size: 0;
+}
+.demo-table-expand label {
+  width: 90px;
+  color: #99a9bf;
+}
+.demo-table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  width: 50%;
+}
+.el-table--border th:first-child .cell, .el-table--border td:first-child .cell{display:inline-block;}
+.add .el-table__expand-icon {
+  width: 32px;
+  height: 32px;
+  background: url("../../icons/img/jiahao.png");
+  -webkit-transform: rotate(0deg);
+  transform: rotate(0deg);
+  display:inline-block;
+}
+.add .el-table__expand-icon > .el-icon {
+  display: none;
+}
+.jian {width: 32px;
+  height: 32px;
+  background: url("../../icons/img/jianhao.png");
+  -webkit-transform: rotate(0deg);
+  transform: rotate(0deg);}
+.el-date-editor.el-input,
+.el-date-editor.el-input__inner {
+  width: 100%;
+}
+.red input {
+  background-color: red;
+}
+.pink input {
+  background-color: pink;
+}
+
+.tableClass {
+  color: #666;
+  border-color: #666;
+}
+</style>
